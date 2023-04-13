51ee4bdb66fc53f111c33b3fffdc0033751b2363
princejoogie <princejoogie@gmail.com>
2023-04-13 12:49:43 +0800

    chore: dont validate on client

--- apps/web/src/pages/api/hooks/clerk.ts
+++ apps/web/src/pages/api/hooks/clerk.ts
@@ -1,25 +1,22 @@
-import { eventSchema, createOrUpdateSchema, type EventData } from "api";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { httpApi } from "../../../utils/api";
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<EventData>
-) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
     return res.status(405).json({ code: 405, message: "Method not allowed" });
   }
 
-  const base = eventSchema.parse(req.body);
-  if (base.type === "user.created" || base.type === "user.updated") {
-    const data = createOrUpdateSchema.parse(base.data);
-    await httpApi.user.upsert.mutate(data);
-    return res.status(200).json(data);
-  }
+  /* const base = eventSchema.parse(req.body); */
+  if (req.body.type && typeof req.body.type === "string") {
+    if (req.body.type === "user.created" || req.body.type === "user.updated") {
+      const data = await httpApi.user.upsert.mutate(req.body.data);
+      return res.status(200).json(data);
+    }
 
-  if (base.type === "user.deleted") {
-    await httpApi.user.delete.mutate(base);
-    return res.status(200).json(base);
+    if (req.body.type === "user.deleted") {
+      const data = await httpApi.user.delete.mutate(req.body.data);
+      return res.status(200).json(data);
+    }
   }
 
   return res.status(400).json({ code: 400, message: "Bad request" });
