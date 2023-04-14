0d034bb19c83e844b621244dffaf508296f347ac
princejoogie <princejoogie@gmail.com>
2023-04-14 11:07:45 +0800

    chore: doc

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,46 +1,64 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  type DocumentContext,
+  type DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link
-          rel="manifest"
-          crossOrigin="use-credentials"
-          href="/manifest.json"
-        />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#FFFFFF" />
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
 
-        <meta name="description" content="It's just YouTube.. but text!" />
-        <meta property="og:title" content="ChunkTube" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://chunktube.tech" />
-        <meta property="og:image" content="/api/og" />
-        <meta
-          property="og:description"
-          content="It's just YouTube.. but text!"
-        />
-        <meta property="og:site_name" content="ChunkTube" />
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link
+            rel="manifest"
+            crossOrigin="use-credentials"
+            href="/manifest.json"
+          />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#FFFFFF" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:domain" content="chunktube.tech" />
-        <meta name="twitter:url" content="https://chunktube.tech" />
-        <meta name="twitter:creator" content="@princecaarlo" />
-        <meta name="twitter:title" content="ChunkTube" />
-        <meta
-          name="twitter:description"
-          content="It's just YouTube.. but text!"
-        />
-        <meta name="twitter:image" content="/api/og" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+          <meta name="description" content="It's just YouTube.. but text!" />
+          <meta property="og:title" content="ChunkTube" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://chunktube.tech" />
+          <meta property="og:image" content="/api/og" />
+          <meta
+            property="og:description"
+            content="It's just YouTube.. but text!"
+          />
+          <meta property="og:site_name" content="ChunkTube" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:domain" content="chunktube.tech" />
+          <meta name="twitter:url" content="https://chunktube.tech" />
+          <meta name="twitter:creator" content="@princecaarlo" />
+          <meta name="twitter:title" content="ChunkTube" />
+          <meta
+            name="twitter:description"
+            content="It's just YouTube.. but text!"
+          />
+          <meta name="twitter:image" content="/api/og" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
