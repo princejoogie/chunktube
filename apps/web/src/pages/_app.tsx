68816f8749b176842d98170e5c68ba24ba904960
princejoogie <princejoogie@gmail.com>
2023-04-16 23:33:44 +0800

    feat: add gtag and analytics

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import "~/styles/globals.css";
 import Script from "next/script";
-import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { type AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
+import * as gtag from "~/lib/gtm";
 import { api } from "~/utils/api";
 import { Toaster } from "~/components/toaster";
 
 /* export const queryClient = new QueryClient(); */
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageView(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
   return (
     <ClerkProvider
       {...pageProps}
@@ -19,6 +34,26 @@ const App = ({ Component, pageProps }: AppProps) => {
         elements: { userButtonPopoverCard: "border-2 border-gray-600" },
       }}
     >
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){
+console.log("args:", arguments);
+dataLayer.push(arguments);
+}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Script
         async
         data-ad-client="ca-pub-5644238710712581"
