ca29c4730bf6787f8d0e629df48e0163699637cd
princejoogie <princejoogie@gmail.com>
2023-04-16 20:30:35 +0800

    chore: add sitemap, robots, and remove auth middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,46 +1,23 @@
-import { withClerkMiddleware, getAuth } from "@clerk/nextjs/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-const assets = [
-  "/android-chrome-192x192.png*",
-  "/android-chrome-512x512.png*",
-  "/apple-touch-icon.png*",
-  "/favicon-16x16.png*",
-  "/favicon-32x32.png*",
-  "/favicon.ico*",
-  "/manifest.json*",
-  "/ads.txt*",
-  "/assets*",
-];
+/* const assets = [ */
+/*   "/android-chrome-192x192.png*", */
+/*   "/android-chrome-512x512.png*", */
+/*   "/apple-touch-icon.png*", */
+/*   "/favicon-16x16.png*", */
+/*   "/favicon-32x32.png*", */
+/*   "/favicon.ico*", */
+/*   "/manifest.json*", */
+/*   "/ads.txt*", */
+/*   "/assets*", */
+/* ]; */
 
-// Set the paths that don't require the user to be signed in
-const publicPaths = ["/", "/sign-in*", "/sign-up*", "/api*", ...assets];
-
-const isPublic = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-};
-
-export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next();
-  }
-
-  // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(request);
-
-  if (!userId) {
-    // redirect the users to /pages/sign-in/[[...index]].ts
-    const signInUrl = new URL("/sign-in", request.url);
-    signInUrl.searchParams.set("redirect_url", request.url);
-    return NextResponse.redirect(signInUrl);
-  }
-
+export default withClerkMiddleware(() => {
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher:
+    "/((?!_next/image|_next/static|api/og|favicon.ico|ads.txt|manifest.json|sitemap.xml|robots.txt|assets).*)",
 };
