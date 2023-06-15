479f5c1dc52f3acaeb15664001384e818af3328e
princejoogie <princejoogie@gmail.com>
2023-06-15 15:21:26 +0800

    chore: gtm

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -58,6 +58,27 @@ const App = ({ Component, pageProps }: AppProps) => {
         crossOrigin="anonymous"
         onError={console.error}
       />
+      <Script
+        id="gtm-head"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WPRQ3X6');
+            `,
+        }}
+      />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WPRQ3X6"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
       {/* @ts-expect-error - types  */}
       <Component {...pageProps} />
       <div className="h-48 w-full" />
