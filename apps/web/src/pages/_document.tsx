863834167d438f3a83ad07944ff7d01d76f15635
princejoogie <princejoogie@gmail.com>
2023-06-15 15:42:17 +0800

    chore: add no script

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -54,14 +54,18 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-WPRQ3X6"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe
+                  src="https://www.googletagmanager.com/ns.html?id=GTM-WPRQ3X6"
+                  height="0"
+                  width="0"
+                  style="display:none;visibility:hidden"
+                />
+              `,
+            }}
+          ></noscript>
           <NextScript />
         </body>
       </Html>
